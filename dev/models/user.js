df2940302b84200e03a735bc5f03ab74f0589224
ammar1019 <ammarkhan.in2020@gmail.com>
2025-07-06 23:24:18 +0530

    Update user.js

--- dev/models/user.js
+++ dev/models/user.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
-// Hash password before saving
+
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
 
@@ -36,7 +36,7 @@ userSchema.pre('save', async function (next) {
     }
 });
 
-// Compare password for login
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
